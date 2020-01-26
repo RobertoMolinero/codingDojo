33b1c02186f584d69415403175a05cfe7f7f377a
Roberto Molinero <kisshu80@googlemail.com>
2020-01-26 13:50:43 +0100

    FizzBuzz (Step 3+4 of 4)

--- 01 kata/00 function/00 fizzBuzz/fizzBuzz.go
+++ 01 kata/00 function/00 fizzBuzz/fizzBuzz.go
@@ -4,10 +4,22 @@ import "fmt"
 
 func main() {
 	to100 := generateSequenceFrom1To100()
-	f := fizz(to100)
 
-	for idx, i := range f {
-		fmt.Printf("Number [%d]: %s\n", idx, i)
+	resultOfFizz := fizz(to100)
+	resultOfBuzz := buzz(to100)
+
+	for idx, i := range to100 {
+		fmt.Printf("Number [%d]:", i)
+		fmt.Printf("\t")
+
+		if len(resultOfFizz[idx])+len(resultOfBuzz[idx]) > 0 {
+			fmt.Printf("%s", resultOfFizz[idx])
+			fmt.Printf("%s", resultOfBuzz[idx])
+		} else {
+			fmt.Print(i)
+		}
+
+		fmt.Printf("\n")
 	}
 }
 
@@ -15,7 +27,6 @@ func generateSequenceFrom1To100() []int {
 	start := 1
 	max := 100
 	step := 1
-
 	s := make([]int, max)
 	for i := range s {
 		s[i] = start
@@ -25,15 +36,19 @@ func generateSequenceFrom1To100() []int {
 }
 
 func fizz(s []int) []string {
+	return substituteMultiples(s, 3, "Fizz")
+}
 
+func buzz(s []int) []string {
+	return substituteMultiples(s, 5, "Buzz")
+}
+
+func substituteMultiples(s []int, factor int, word string) []string {
 	t := make([]string, len(s))
 	for idx, i := range s {
-		if i%3 == 0 {
-			t[idx] = "fizz"
-		} else {
-			t[idx] = fmt.Sprint(i)
+		if i%factor == 0 {
+			t[idx] = word
 		}
 	}
-
 	return t
 }
--- 01 kata/00 function/00 fizzBuzz/fizzBuzz_test.go
+++ 01 kata/00 function/00 fizzBuzz/fizzBuzz_test.go
@@ -21,12 +21,12 @@ func Test_generateSequenceFrom1To100(t *testing.T) {
 	got := generateSequenceFrom1To100()
 
 	if len(got) != 100 {
-		t.Errorf("Length of sequence is %v, want %v", len(got), 100)
+		t.Errorf("Length of sequence is %d, want %d", len(got), 100)
 	}
 
 	for _, tt := range tests {
 		if got[tt.index] != tt.result {
-			t.Errorf("%s: %v, want %v", tt.name, got[tt.index], tt.result)
+			t.Errorf("%s: %d, want %d", tt.name, got[tt.index], tt.result)
 		}
 	}
 }
@@ -37,17 +37,41 @@ func Test_fizz(t *testing.T) {
 		args []int
 		want []string
 	}{
-		{"First 3 Numbers", []int{1, 2, 3}, []string{"1", "2", "fizz"}},
-		{"Last 3 Numbers", []int{98, 99, 100}, []string{"98", "fizz", "100"}},
+		{"First 3 Numbers", []int{1, 2, 3}, []string{"", "", "Fizz"}},
+		{"Middle of the sequence", []int{48, 49, 50, 51, 52}, []string{"Fizz", "", "", "Fizz", ""}},
+		{"Last 3 Numbers", []int{98, 99, 100}, []string{"", "Fizz", ""}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := fizz(tt.args); !reflect.DeepEqual(len(got), len(tt.want)) {
-				t.Errorf("Length of fizz() = %v, want %v", len(got), len(tt.want))
+				t.Errorf("Length of fizz() = %d, want %d", len(got), len(tt.want))
 			}
 			if got := fizz(tt.args); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("fizz() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buzz(t *testing.T) {
+	tests := []struct {
+		name string
+		args []int
+		want []string
+	}{
+		{"First 5 Numbers", []int{1, 2, 3, 4, 5}, []string{"", "", "", "", "Buzz"}},
+		{"Middle of the sequence", []int{48, 49, 50, 51, 52}, []string{"", "", "Buzz", "", ""}},
+		{"Last 5 Numbers", []int{96, 97, 98, 99, 100}, []string{"", "", "", "", "Buzz"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fizz(tt.args); !reflect.DeepEqual(len(got), len(tt.want)) {
+				t.Errorf("Length of fizz() = %d, want %d", len(got), len(tt.want))
+			}
+			if got := buzz(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buzz() = %v, want %v", got, tt.want)
 			}
 		})
 	}
